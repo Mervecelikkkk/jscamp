0a803176bdc4fa9c245f2aa18b0460e995262aee
mervecelikkkk <merve.celik56@outlook.com>
2021-10-19 00:55:50 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,16 @@
+console.log("Merhaba Kodlama.io")
+
+//array
+//camelCasing
+//pascalCasing
+var dolarBugun=9.20
+var dolarDun=9.30
+console.log(dolarDun)
+
+let konutKredileri=["Konut Kredisi","Emlak KOnut Kredisi","Kamu Konut Kredisi"]
+console.log("<ul>")
+for (let i = 0; i <konutKredileri.length; i++) {
+    //const element = array[index];
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")

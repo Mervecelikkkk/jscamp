0d79f9fb73ae01ff40f39f0114b7c700a30458ce
mervecelikkkk <merve.celik56@outlook.com>
2021-11-26 15:04:45 +0300

    add validation to data

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,19 +1,119 @@
-export default class UserService{
-     constructor(loggerService){
-         this.users=[]
-         this.loggerService=loggerService
-     }
-     
-   
-    add(user){
-       this.users.push(user)
-       this.loggerService.log(user)
+import { users } from "../data/users.js"
+import DataError from "../models/dataError.js"
+
+export default class UserService {
+    constructor(loggerService) {
+        this.employees = []
+        this.customers = []
+        this.errors = []
+        this.loggerService = loggerService
+    }
+
+    load() {
+        for (const user of users) {
+            switch (user.type) {
+
+                case "customer":
+                    if (!this.checkCustomerValidityForErrors(user)) {
+                        this.customers.push(user)
+                    }
+
+                    break;
+                case "employee":
+                    if (!this.checkEmployeeValidityForErrors(user)) {
+                        this.employees.push(user)
+                    }
+                    break;
+
+                default:
+                    this.errors.push(new DataError("Wrong user type", user))
+                    break;
+            }
+        }
+    }
+    //formik-yup
+    checkCustomerValidityForErrors(user) {
+        let requiredFields = "id firstName lastName age city creditCardNumber".split(" ")
+        let hasErrors = false
+        for (const field of requiredFields) {
+            if (!user[field]) {
+                hasErrors = true
+                this.errors.push(
+                    new DataError(`Validation problem. ${field} is required`, user))
+            }
+        }
+        if (Number.isNaN(Number.parseInt(+user.age))) {
+            hasErrors = true
+            this.errors.push(new DataError(`Validation problem. ${user.age} is not a number.`, user))
+        }
+        return hasErrors
 
     }
-    list(){
-        return this.users  
+    checkEmployeeValidityForErrors(user) {
+        let requiredFields = "id firstName lastName age city salary".split(" ")
+        let hasErrors = false
+        for (const field of requiredFields) {
+            if (!user[field]) {
+                hasErrors = true
+                this.errors.push(
+                    new DataError(`Validation problem. ${field} is required`, user))
+            }
+        }
+        if (Number.isNaN(Number.parseInt(+user.age))) {
+            hasErrors = true
+            this.errors.push(new DataError(`Validation problem. ${user.age} is not a number.`, user))
+        }
+        return hasErrors
+
     }
-    getById(id){
-      return this.users.find(u=>u.id===id)
+
+    add(user) {
+        switch (user.type) {
+            case "customer":
+                if (!this.checkCustomerValidityForErrors(user)) {
+                    this.customers.push(user)
+                }
+                break;
+            case "employee":
+                if (!this.checkEmployeeValidityForErrors(user)) {
+                    this.employees.push(user)
+                }
+                break;
+
+            default:
+                this.errors.push(new DataError("This user can not be added. Wrong user type ", user))
+                break;
+        }
+        this.loggerService.log(user)
+
     }
+    listCustomers() {
+        return this.customers
+    }
+    getCustomersById(id) {
+        return this.customers.find(u => u.id === id)
+    }
+
+    listEmployees() {
+        return this.employees
+    }
+    getEmployeesById(id) {
+        return this.employees.find(u => u.id === id)
+    }
+    //Sorting
+    getCustomersSorted() {
+      return this.customers.sort((customer1, customer2) => {
+            if (customer1.firstName < customer2.firstName) { 
+                return -1
+             }
+            else if (customer1.firstName === customer2.firstName){
+                return 0
+            }else 
+            {
+                return 1
+            }
+    })
+}
+
+
 }
